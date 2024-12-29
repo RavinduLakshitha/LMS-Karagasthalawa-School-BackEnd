63581312b8eaa9b206fde552e63d2aae5cae0288
kushana <rashmisenadheera98@gmail.com>
2024-12-30 00:21:58 +0530

    created path in route and called in server

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const bookController = require('../Controllers/bookController');
 
 router.post('/submit-book', bookController.submitBook);
-
+router.get('/display-book', bookController.displayBook );
 module.exports = router;
