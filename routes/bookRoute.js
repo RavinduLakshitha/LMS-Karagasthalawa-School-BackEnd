8a88b87d61769cc100c20b8b914f06764aa5e7c1
ravindu <ravindulakshitha250@gmail.com>
2025-01-06 09:08:27 +0530

    Add ReturnBook Api

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bookController = require('../Controllers/bookController');
+const bookController = require("../Controllers/bookController");
 
-router.post('/submit-book', bookController.submitBook);
-router.get('/display-book', bookController.displayBook );
-router.put('/api/update-book/:id', bookController.updateBook);
-
+router.post("/submit-book", bookController.submitBook);
+router.get("/display-book", bookController.displayBook);
+router.put("/api/update-book/:id", bookController.updateBook);
+router.put("/return-book", bookController.returndisplayBook);
 
 module.exports = router;
